ac39f2f8dd6737a27ea98cbd302aa335c8bb8ff9
OM M PATEL <Om.pce21@sot.pdpu.ac.in>
2025-11-25 00:24:25 +0530

    kinova connectors added

---
+++ kinova/src/greenarm_manipulation/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = "greenarm_manipulation"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages",
+         ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="GreenArm Team",
+    maintainer_email="placeholder@example.com",
+    description="Kinova pick-and-place node that subscribes to SourceTarget coordinates.",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "pickplace_node = greenarm_manipulation.pickplace_node:main",
+        ],
+    },
+)
