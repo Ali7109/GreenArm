fcf2908b617183d72f8848c2ebca09c713b69e3b
Ali Hassan Amin <aliamin745@gmail.com>
2025-11-27 13:31:31 -0500

    simple scripts

---
+++ kinova/detector_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Source ROS2 installation (change for your distro)
+source /opt/ros/humble/setup.bash
+
+# Go to your workspace
+cd ~/your_ws  # change this
+
+# Build
+colcon build
+
+# Source *your* workspace
+source install/setup.bash
+
+# Run the node
+exec ros2 run greenarm_perception source_detector
